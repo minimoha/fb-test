5be8b2487e93f94187be56e8935993a1f8da1cb6
moha <moha>
2023-11-27 22:08:55 -0800

    first commit

---
+++ Test.API/Models/CustomerOtherInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace Test.API.Models
+{
+    public class CustomerOtherInfo
+    {
+        public long Id { get; set; }
+        public string AccountNo { get; set; }
+        public string InvoiceNumber { get; set; }
+        public string Quantity { get; set; }
+        public string DeliveryAddress { get; set; }
+    }
+}
---
+++ Test.API/Models/Industry.cs
@@ -0,0 +1,8 @@
+﻿namespace Test.API.Models
+{
+    public class Industry
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
