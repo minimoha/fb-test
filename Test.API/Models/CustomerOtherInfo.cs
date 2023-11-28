b348e00cbab900549a37403a187f7b1cdc8f0523
moha <moha>
2023-11-27 22:16:13 -0800

    aa

--- Test.API/Models/CustomerOtherInfo.cs
+++ Test.API/Models/CustomerOtherInfo.cs
@@ -4,8 +4,18 @@
     {
         public long Id { get; set; }
         public string AccountNo { get; set; }
-        public string InvoiceNumber { get; set; }
-        public string Quantity { get; set; }
-        public string DeliveryAddress { get; set; }
+        public string Industry { get; set; }
+        public string Field1 { get; set; }
+        public string Field2 { get; set; }
+        public string Field3 { get; set; }
+        //public string InvoiceNumber { get; set; }
+        //public string Quantity { get; set; }
+        //public string DeliveryAddress { get; set; }
+        //public string MatricNumber { get; set; }
+        //public string Level { get; set; }
+        //public string Course { get; set; }
+        //public string GsmNumber { get; set; }
+        //public string Network { get; set; }
+        //public string ResidentialAddress { get; set; }
     }
 }
